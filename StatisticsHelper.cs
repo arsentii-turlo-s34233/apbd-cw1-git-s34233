4510da84a58a4346d3f97aa3ce239f4d44c1b776
Arsentii Turlo <s34233@pjwstk.edu.pl>
2026-03-17 13:41:25 +0100

    Add CalculateAverage method

--- StatisticsHelper.cs
+++ StatisticsHelper.cs
@@ -2,5 +2,14 @@ namespace apbd_cw1_git_s34233;
 
 public class StatisticsHelper
 {
-    
+    public static double CalculateAverage(int[] numbers)
+    {
+        int sum = 0;
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            sum+=numbers[i];
+        }
+        double result = (double) sum / numbers.Length;
+        return result;
+    }
 }
