914fbddab6f49767444f0def3bd7c44aac50cff4
Arsentii Turlo <s34233@pjwstk.edu.pl>
2026-03-17 13:30:27 +0100

    Added basic user input handling to Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Enter integers in a one line separated by commas (',')");
+string input = Console.ReadLine();
+string[] input_pieces = input.Split(',');
+int[] numbers = new int[input_pieces.Length];
+for (int i = 0; i< numbers.Length; i++)
+{
+    numbers[i] = int.Parse(input_pieces[i]);
+}
